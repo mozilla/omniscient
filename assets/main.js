87dfa3a3ca0089402aa7c68c96d9a353e0cc9bfb
Jackie Munroe <jackiemunroe@users.noreply.github.com>
2019-04-22 12:55:20 -0700

    Removed hard coding of field id for fxa uid to allow for more flexibility should the field id change in Zendesk. Set up templating for displaying the subscription data

--- assets/main.js
+++ assets/main.js
@@ -3,13 +3,15 @@ $(function () {
     client.invoke('resize', {width: '100%', height: '200px'});
 
     //Fetch the Firefox Account ID from the ticket
-    client.get('ticket.customField:custom_field_360018061392').then(
-        function (data) {
-            var fxa_id = data['ticket.customField:custom_field_360018061392'];
+    client.metadata().then(function (metadata) {
+        var fxaFieldId = metadata.settings.fxaFieldId;
+        var fxaProperty = 'ticket.customField:custom_field_' + fxaFieldId;
 
+        client.get(fxaProperty).then(function (data) {
+            var fxa_id = data[fxaProperty];
             getSubscriptionInfo(client, fxa_id);
-        }
-    )
+        })
+    })
 })
 
 function getSubscriptionInfo(client, fxa_id) {
@@ -29,8 +31,7 @@ function getSubscriptionInfo(client, fxa_id) {
 
         client.request(settings).then(
             function(data) {
-                console.log(data);
-                showSubscriptionInfo(data);
+                showSubscriptionInfo(data, fxa_id);
             },
             function(response) {
                 console.error(response.responseText);
@@ -39,9 +40,45 @@ function getSubscriptionInfo(client, fxa_id) {
     })
 }
 
-function showSubscriptionInfo(data) {
-    for (var i = 0; i < data.length; i++) {
-        var subsObj = data[1];
-        console.log(subsObj);
-    }
+function showSubscriptionInfo(data, fxa_id) {
+    var subscriptions = [];
+    data.forEach(function (obj) {
+        console.log(obj);
+
+        var status = 'active';
+        if (obj.end_at != null) {
+            status = 'cancelled';
+        }
+
+        var nextChargeDate = new Date(obj.current_period_end * 1000);
+
+        var subscription = {
+            'subscriptionId': obj.subscription_id,
+            'nextChargeDate': nextChargeDate,
+            'status': status,
+        };
+
+        subscriptions.push(subscription);
+    });
+
+    var subscriptionData = {
+        'user': fxa_id,
+        'subscriptions': subscriptions
+    };
+
+    console.log(subscriptionData);
+
+    var source = $("#sub-template").html();
+    var template = Handlebars.compile(source);
+    var html = template(subscriptionData);
+    $("#content").html(html);
 }
+
+Handlebars.registerHelper('list', function(context, options) {
+    var ret = "<ul>";
+    for(var i=0, j=context.length; i<j; i++) {
+        ret = ret + "<li>" + options.fn(context[i]) + "</li>";
+    }
+
+    return ret + "</ul>";
+});
